baccd291fb644ad13022eb59ba651f48c655f0c5
Charlie Blackstock <charlieblackstock@gmail.com>
2017-01-09 13:42:22 -0700

    step 2

--- src/index.js
+++ src/index.js
@@ -1,39 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
-const API_URL = 'http://www.omdbapi.com/?s='
-import './stylesheets/index.css'
 
-import SearchBar from './components/SearchBar';
-import MovieList from './components/MovieList';
-
-class App extends Component {
-  constructor() {
-    super()
-
-    this.state = {
-      movies: []
-    }
-
-    this.searchMovie('cool');
-  }
-
-  searchMovie(term) {
-    axios.get(`${API_URL}${term}`)
-      .then(res => {
-        this.setState({ movies: res.data.Search });
-      });
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <h1>OMDB Movie Search</h1>
-        <SearchBar searchMovie={this.searchMovie.bind(this)} />
-        <MovieList movies={this.state.movies} />
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div className="container">
+      <h1>OMDB Movie Search</h1>
+    </div>
+  )
 }
 
 ReactDOM.render(
