d8421c95fd66d7b93a1210af845a47e2aa162379
Charlie Blackstock <charlieblackstock@gmail.com>
2017-01-10 08:27:06 -0700

    step11

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,30 +2,22 @@ import React, { Component } from 'react';
 import '../stylesheets/SearchBar.css'
 
 class SearchBar extends Component {
-    constructor(props) {
-      super(props);
+    constructor() {
+      super();
 
       this.state = { term: '' };
     }
 
     render() {
+      console.log(this.state);
       return (
         <div className="search-bar input-group">
           <input
-            value={this.state.term}
             className="form-control"
             placeholder="Search Movie Title..."
-            onChange={event => this.setState({ term: event.target.value })}
+            value={this.state.term}
+            onChange={(event) => this.setState({ term: event.target.value })}
             />
-          <span className="input-group-btn">
-            <button
-              className="btn btn-success"
-              type="button"
-              onClick={() => this.props.searchMovie(this.state.term)}
-              >
-              Search!
-            </button>
-          </span>
         </div>
       );
     }
